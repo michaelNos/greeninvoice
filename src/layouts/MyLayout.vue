7bd07eceb5d76fe360027f7f7ea46e0a313b48d8
michaelNos <nos.michael@gmail.com>
2018-09-17 16:15:27 +0300

    quasar starterkit app after moderate changes

---
+++ src/layouts/MyLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar color="primary">
+        <q-toolbar-title>
+          <q-icon name="lock_outline" />
+          Logged out
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card>
+      <q-card-title>
+        Log in
+      </q-card-title>
+      <q-card-separator />
+      <q-card-main>
+        <q-field
+          icon="vpn_key"
+          label="API key"
+          orientation="vertical"
+        >
+          <q-input v-model="api_key" />
+        </q-field>
+      </q-card-main>
+      <q-card-main>
+        <q-field
+          icon="security"
+          label="API secret"
+          orientation="vertical"
+        >
+          <q-input v-model="api_secret" />
+        </q-field>
+      </q-card-main>
+      <q-card-main classes="flex flex-center">
+        <q-btn style="width: 100%" size="xl" color="secondary">
+          <q-icon name="get_app" />Submit
+        </q-btn>
+      </q-card-main>
+    </q-card>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  props: {
+    api_key: {
+      type: String,
+      default: ''
+    },
+    api_secret: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
